78674d6b59a5244d98a8d05cf9c5ac7ff908de78
ahmet-serdar <baykalaserdar@gmail.com>
2025-09-02 17:17:11 +0200

    chage config

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
-import path from "path";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    // @ts-expect-error Next.js types henüz bu alanı bilmiyor
-    outputFileTracingRoot: path.join(__dirname, "../../"),
-  },
+  
 };
 
 export default nextConfig;
